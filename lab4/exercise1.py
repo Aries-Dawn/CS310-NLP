dc86814e31e5151619b5e0e5fe13bc178765975b
CS310-NLP <3055637042@qq.com>
2021-03-28 05:19:50 +0800

    Finish Lab4 exercise1

--- lab4/exercise1.py
+++ lab4/exercise1.py
@@ -1,5 +1,17 @@
 import jieba
+
 print('/'.join(jieba.cut('江州市长江大桥参加了长江大桥的通车仪式', HMM=False)))
-for i in range(9991*9):
-    jieba.suggest_freq(('长江','大桥'), True)
+jieba.add_word('江大桥',20000)
 print('/'.join(jieba.cut('江州市长江大桥参加了长江大桥的通车仪式', HMM=False)))
+
+print()
+
+print('/'.join(jieba.cut('我們在野生動物園玩', HMM=False)))
+jieba.add_word('我們')
+jieba.add_word('野生動物園')
+print('/'.join(jieba.cut('我們在野生動物園玩', HMM=False)))
+
+
+print('/'.join(jieba.cut('ETF 新兵登场元大宝来推 ETF 伞型证券投资信托基金', HMM=False)))
+jieba.add_word('元大宝来')
+print('/'.join(jieba.cut('ETF 新兵登场元大宝来推 ETF 伞型证券投资信托基金', HMM=False)))
